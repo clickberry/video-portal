91eb776389a2c30af2674f2be3908fd2bf0d874e
alexey <a.melnikov@clickberry.com>
2015-02-18 13:42:45 +0300

    License updated

--- Source/Entities/Portal.Domain/BackendContext/Constant/HeaderParameters.cs
+++ Source/Entities/Portal.Domain/BackendContext/Constant/HeaderParameters.cs
@@ -1,5 +1,5 @@
 // Copyright (c) Clickberry, Inc. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Licensed under the GNU GENERAL PUBLIC LICENSE Version 3. See License.txt in the project root for license information.
 
 namespace Portal.Domain.BackendContext.Constant
 {
--- Source/FrontEnd/Portal.Web/Scripts/plugins/jquery.placeholder.js
+++ Source/FrontEnd/Portal.Web/Scripts/plugins/jquery.placeholder.js
@@ -2,7 +2,7 @@
  * placeholder - HTML5 input placeholder polyfill
  * Copyright (c) 2012 DIY Co
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * Licensed under the GNU GENERAL PUBLIC LICENSE Version 3 (the "License"); you may not use this
  * file except in compliance with the License. You may obtain a copy of the License at:
  * http://www.apache.org/licenses/LICENSE-2.0
  *
